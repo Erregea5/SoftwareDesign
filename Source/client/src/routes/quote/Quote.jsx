0bc3544c88810894ae71de5e52c04b297aaef7f0
lindolfo1 <cuentas@feptec.com>
2024-02-23 16:37:58 -0600

    new file:   Source/client/src/routes/quote/CryptowalletStyles.css 	modified:   Source/client/src/routes/quote/Quote.jsx 	modified:   Source/client/src/routes/quote/components/FuelQuoteForm.jsx 	modified:   Source/client/src/routes/quote/components/QuoteResult.jsx

--- Source/client/src/routes/quote/Quote.jsx
+++ Source/client/src/routes/quote/Quote.jsx
@@ -1,5 +1,31 @@
+// import React, { useState } from "react";
+// import FuelQuoteForm from "./components/FuelQuoteForm";
+
+// const Quote = () => {
+//   // Define handleSubmit function
+//   const handleSubmit = (gallonsRequested, deliveryDate) => {
+//     // Perform form submission logic here
+//     console.log("Form submitted with gallonsRequested:", gallonsRequested);
+//     console.log("Delivery date:", deliveryDate);
+//     // can perform additional actions here, such as calling an API or updating state
+//   };
+
+//   return (
+//     <div>
+//       <h1>Fuel Quote Form</h1>
+//       <FuelQuoteForm
+//         deliveryAddress="123 Main St, City, State, ZIP"
+//         handleSubmit={handleSubmit}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Quote;
+
 import React, { useState } from "react";
 import FuelQuoteForm from "./components/FuelQuoteForm";
+import "./CryptowalletStyles.css";
 
 const Quote = () => {
   // Define handleSubmit function
@@ -11,7 +37,9 @@ const Quote = () => {
   };
 
   return (
-    <div>
+    <div className="card">
+      {" "}
+      {/* Wrap the FuelQuoteForm inside a div with the 'card' class */}
       <h1>Fuel Quote Form</h1>
       <FuelQuoteForm
         deliveryAddress="123 Main St, City, State, ZIP"
@@ -22,3 +50,4 @@ const Quote = () => {
 };
 
 export default Quote;
+
--- Source/client/src/routes/quote/components/QuoteResult.jsx
+++ Source/client/src/routes/quote/components/QuoteResult.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../CryptowalletStyles.css";
 
 const QuoteResult = ({ price, numGal }) => {
   return (
