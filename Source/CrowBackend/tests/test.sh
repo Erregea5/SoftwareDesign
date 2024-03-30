aaffb46bcc00ddd57a3b8e9db86b6f6f367f555a
lindolfo1 <cuentas@feptec.com>
2024-03-29 23:31:51 -0500

    modified:   Source/CrowBackend/tests/test.sh

--- Source/CrowBackend/tests/test.sh
+++ Source/CrowBackend/tests/test.sh
@@ -9,3 +9,19 @@ g++ -fdiagnostics-color=always \
 
 chmod +x ./test.out
 ./test.out
+
+# Mac:
+# sh ./test.out
+
+# Linux:
+# g++ -fdiagnostics-color=always \
+#     -std=c++2a \
+#     -g test.cpp ../src/server.cpp ../src/Client.cpp ../src/FuelQuote.cpp ../src/Routes.cpp \
+#     -o test.out \
+#     -I../dependencies/asio/include \
+#     -I../dependencies/Crow/include \
+#     -I../dependencies/catch2/include \
+#     -lpthread
+
+# chmod +x ./test.out
+# ./test.out
